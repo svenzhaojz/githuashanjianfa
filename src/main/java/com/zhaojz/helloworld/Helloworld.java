1ff1ba36bb5749a33627e6ab265261da37f5634c
zhaojz <svenzhaojz@126.com>
2019-04-27 21:22:56 +0800

    增加test.class

--- src/main/java/com/zhaojz/helloworld/Helloworld.java
+++ src/main/java/com/zhaojz/helloworld/Helloworld.java
@@ -3,5 +3,7 @@ package com.zhaojz.helloworld;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world11111");
+        System.out.println("hello world22222");
     }
 }
