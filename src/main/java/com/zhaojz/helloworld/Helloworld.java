2a4638263a9f49d16f69ef3cd797173014c5200d
SunnyQingZhao <50035845+SunnyQingZhao@users.noreply.github.com>
2019-04-27 23:58:47 +0800

    SunnyTest

--- src/main/java/com/zhaojz/helloworld/Helloworld.java
+++ src/main/java/com/zhaojz/helloworld/Helloworld.java
@@ -5,5 +5,6 @@ public class Helloworld {
         System.out.println("hello world");
         System.out.println("hello world11111");
         System.out.println("hello world22222");
+        System.out.println("sunny update");
     }
 }
